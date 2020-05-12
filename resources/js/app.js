046d1f7dbd8958d0b484c0c35f2ab3eeefb4d2ba
Heitor Quaglia <heitor.bq@hotmail.com>
2020-05-12 18:46:36 -0300

    users component and route

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,12 +7,13 @@ Vue.use(VueRouter)
 
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard').default },
-    { path: '/profile', component: require('./components/Profile').default }
+    { path: '/profile', component: require('./components/Profile').default },
+    { path: '/users', component: require('./components/Users').default }
 ]
 
 const router = new VueRouter({
     mode: 'history',
-    routes: routes
+    routes
 })
 
 const app = new Vue({
---
+++ resources/js/components/Users.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        USERS
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Profile"
+    }
+</script>
+
+<style scoped>
+
+</style>
